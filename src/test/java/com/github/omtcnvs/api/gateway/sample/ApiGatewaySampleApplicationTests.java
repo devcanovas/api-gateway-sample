b684a124a03ce5a401e32b461b245044bd672cab
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-23 14:30:35 -0300

    Creating project

---
+++ src/test/java/com/github/omtcnvs/api/gateway/sample/ApiGatewaySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.api.gateway.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
